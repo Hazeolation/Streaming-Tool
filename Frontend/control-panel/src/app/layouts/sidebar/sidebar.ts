ed99f07aebee1db1b03cd046cd8259fec24d8ba6
Hazeolation <dweissbrod1410@gmail.com>
2026-05-29 18:30:12 +0200

    Change Scoreboard design

--- Frontend/control-panel/src/app/layouts/sidebar/sidebar.ts
+++ Frontend/control-panel/src/app/layouts/sidebar/sidebar.ts
@@ -1,14 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 import { BroadcastStateService } from '../../services/broadcast-state';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [FormsModule],
+  imports: [FormsModule, NgFor],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
   stateService = inject(BroadcastStateService);
   state = this.stateService.state;
+  availableDivisions = this.stateService.availableDivisions;
 }
--- Frontend/control-panel/src/app/models/broadcast-state.ts
+++ Frontend/control-panel/src/app/models/broadcast-state.ts
@@ -22,5 +22,8 @@ export interface BroadcastState {
 
     // Maps
     maps: MapState[];
-    
+
+    // League Information
+    season: number;
+    division: number;
 }
