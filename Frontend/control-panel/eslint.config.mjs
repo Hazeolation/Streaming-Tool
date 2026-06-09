a3ae6db0f8f3782830cc508157a940a2ce5e7810
Raphi <raphael.hespeler@gmail.com>
2026-06-09 21:57:35 +0200

    Add eslint config file

---
+++ Frontend/control-panel/eslint.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+    {
+        files: [
+            "**/*.ts"
+        ]
+    },
+    {
+        ignores: [
+            "./node_modules/",
+            "./.vscode/",
+            "./.angular/"
+        ]
+    },
+    {
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended
+        ]
+    }
+]);
