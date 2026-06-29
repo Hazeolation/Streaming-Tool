cc7aa00a91135de5b9ad035e345cbb3083b33276
Raphi <raphael.hespeler@gmail.com>
2026-06-29 15:19:56 +0200

    Add resizable text component
    Uses inline svg and dynamic calculation of text bounding box to adjust font size on overflow

---
+++ Frontend/control-panel/src/app/features/resizable-text/resizable-text.ts
@@ -0,0 +1,61 @@
+import {
+  afterRenderEffect,
+  Component,
+  ElementRef,
+  input,
+  OnDestroy,
+  signal,
+  ViewChild,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-resizable-text',
+  imports: [],
+  templateUrl: './resizable-text.html',
+  styleUrl: './resizable-text.scss',
+})
+export class ResizableText implements OnDestroy {
+  /**
+   * Text svg element to calculate dynamic text size correctly when text overflows
+   */
+  @ViewChild('svgText') private svgText?: ElementRef;
+
+  /**
+   * Input signal for text to display
+   */
+  resizableTextContent = input<string>('');
+
+  /**
+   * Signal for viewbox size to adjust it dynamically
+   */
+  viewBoxSize = signal<number[]>([0, 0, 0, 0]);
+
+  /**
+   * Effect that calculates the bounding box of our text element and sets it on the svg container
+   */
+  private resizeTextEffect = afterRenderEffect(() => {
+    // Call resizable text content signal here to trigger effect
+    this.resizableTextContent();
+
+    if (!this.svgText) return;
+
+    const bbox = this.svgText.nativeElement.getBBox();
+    this.viewBoxSize.set([bbox.x, bbox.y, bbox.width, bbox.height]);
+  });
+
+  /**
+   * Destroy all effects on component
+   */
+  ngOnDestroy(): void {
+    this.resizeTextEffect.destroy();
+  }
+
+  /**
+   * Get text content from input effect
+   */
+  get textContent(): string {
+    if (typeof this.resizableTextContent !== 'function') return '';
+
+    return this.resizableTextContent();
+  }
+}
