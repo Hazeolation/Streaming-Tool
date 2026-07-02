d7349da8ca10570fc2d4cbb33d5420fa0a95b1ea
Raphi <raphael.hespeler@gmail.com>
2026-07-02 14:08:00 +0200

    Remove outdated enum file, not used anymore

--- Frontend/control-panel/src/app/enums/broadcast-channel-types.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Enum for different types of `BroadcastChannel`'s to transmit data between browsers
- */
-export enum BroadcastChannelTypes {
-  CommBoxDisplayEvents = 'CommBoxDisplayEvents',
-}
