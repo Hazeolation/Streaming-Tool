daeedb7a429832f497674342e61bb5cf39a1dc43
Raphi <raphael.hespeler@gmail.com>
2026-07-02 09:07:35 +0200

    Add enum for broadcastchannel types

---
+++ Frontend/control-panel/src/app/enums/broadcast-channel-types.ts
@@ -0,0 +1,6 @@
+/**
+ * Enum for different types of `BroadcastChannel`'s to transmit data between browsers
+ */
+export enum BroadcastChannelTypes {
+  CommBoxDisplayEvents = 'CommBoxDisplayEvents',
+}
