c37a398472a346500d02bc8cf1f58b0970c7f3ef
Raphi <raphael.hespeler@gmail.com>
2026-07-15 07:50:54 +0200

    Add settings for dialogs in app config

--- Frontend/control-panel/src/app/app.config.ts
+++ Frontend/control-panel/src/app/app.config.ts
@@ -1,15 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { MAT_DIALOG_DEFAULT_OPTIONS, MatDialogConfig } from '@angular/material/dialog';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch()),
+    provideHttpClient(),
+
+    {
+      provide: MAT_DIALOG_DEFAULT_OPTIONS,
+      useValue: { ...new MatDialogConfig(), hasBackdrop: true },
+    },
   ],
 };
